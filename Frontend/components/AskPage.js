f20a550d3562e904f5711a4c45e4b90dd858fbdf
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-08-04 13:28:12 -0400

    added the camera capabilities, flip, picture

--- Frontend/components/AskPage.js
+++ Frontend/components/AskPage.js
@@ -1,21 +1,128 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Camera } from "expo-camera";
+import { CameraType } from "expo-camera/build/Camera.types";
 
-const AskPage = () => {
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [cameraMode, setCameraMode] = useState("picture"); // "picture" or "video"
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera && cameraMode === "picture") {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  };
+
+  const recordVideo = async () => {
+    if (camera && cameraMode === "video") {
+      try {
+        const videoData = await camera.recordAsync();
+        console.log("Video recorded:", videoData.uri);
+      } catch (error) {
+        console.error("Error recording video:", error);
+      }
+    }
+  };
+
+  const flipCamera = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text> No access to camera </Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Ask Page</Text>
-      {/* Add the video recording and implementation as per the assignment */}
+    <View style={{ flex: 1 }}>
+      <Camera
+        ref={(ref) => setCamera(ref)}
+        style={styles.camera}
+        type={type}
+        ratio={"1:1"}
+      />
+
+      {/* Flip Camera Button */}
+      <TouchableOpacity style={styles.flipCameraButton} onPress={flipCamera}>
+        <Text style={styles.buttonText}>Flip Camera</Text>
+      </TouchableOpacity>
+
+      {/* Record Video Button */}
+      <TouchableOpacity style={styles.recordButton} onPress={recordVideo}>
+        <Text style={styles.buttonText}>Record Video</Text>
+      </TouchableOpacity>
+
+      {/* Take Picture Button */}
+      <TouchableOpacity style={styles.takePictureButton} onPress={takePicture}>
+        <Text style={styles.buttonText}>Take Picture</Text>
+      </TouchableOpacity>
+
+      {/* Display Captured Image */}
+      {image && <Image source={{ uri: image }} style={styles.image} />}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
+  camera: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  flipCameraButton: {
+    position: "absolute",
+    bottom: 16,
+    left: 16,
+    backgroundColor: "#007AFF",
+    paddingVertical: 18,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+  },
+  recordButton: {
+    position: "absolute",
+    bottom: 16,
+    right: 16,
+    backgroundColor: "red",
+    paddingVertical: 18,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+  },
+  takePictureButton: {
+    position: "absolute",
+    bottom: 16,
+    backgroundColor: "#007AFF",
+    paddingVertical: 18,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+    alignSelf: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  image: {
+    flex: 1,
+    width: "100%",
   },
 });
-
-export default AskPage;
